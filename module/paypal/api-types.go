8993207d42a87dc0d35abc4c3fa6adcbd74151f1
Aleš <dev.ales.k@gmail.com>
2018-06-12 17:20:59 +0200

    paypal

--- module/paypal/api-types.go
+++ module/paypal/api-types.go
@@ -1,21 +1,40 @@
 package paypal
 
 type Payment struct {
-	Id         string `json:"id,omitempty"`
-	CreateTime string `json:"create_time,omitempty"`
-	UpdateTime string `json:"update_time,omitempty"`
-	State      string `json:"state,omitempty"`
-	Links      []Link `json:"links,omitempty"`
+	// RETURN SUCCESS
+	Id               string            `json:"id,omitempty"`
+	CreateTime       string            `json:"create_time,omitempty"`
+	UpdateTime       string            `json:"update_time,omitempty"`
+	State            string            `json:"state,omitempty"`
+	Links            []Link            `json:"links,omitempty"`
+	RelatedResources []RelatedResource `json:"related_resources,omitempty"`
 
+	// SUBMIT
 	Intent       string        `json:"intent,omitempty"`
 	Payer        Payer         `json:"payer,omitempty"`
 	Transactions []Transaction `json:"transactions,omitempty"`
 	NoteToPayer  string        `json:"note_to_payer,omitempty"`
 	RedirectUrls RedirectUrls  `json:"redirect_urls,omitempty"`
+
+	// ERROR
+	Name            string         `json:"name,omitempty"`
+	Details         []ErrorDetails `json:"details,omitempty"`
+	Message         string         `json:"message,omitempty"`
+	InformationLink string         `json:"information_link,omitempty"`
+	DebugId         string         `json:"debug_id,omitempty"`
 }
 
 type Payer struct {
-	PaymentMethod string `json:"payment_method,omitempty"`
+	PaymentMethod string    `json:"payment_method,omitempty"`
+	PayerInfo     PayerInfo `json:"payer_info,omitempty"`
+}
+
+type PayerInfo struct {
+	Email           string          `json:"email,omitempty"`
+	FirstName       string          `json:"first_name,omitempty"`
+	LastName        string          `json:"last_name,omitempty"`
+	PayerId         string          `json:"payer_id,omitempty"`
+	ShippingAddress ShippingAddress `json:"shipping_address,omitempty"`
 }
 
 type Transaction struct {
@@ -29,8 +48,8 @@ type Transaction struct {
 }
 
 type Amount struct {
-	Total    string        `json:"total,omitempty"` // 30.11
-	Currency string        `json:"total,omitempty"` // USD
+	Total    string        `json:"total,omitempty"`    // 30.11
+	Currency string        `json:"currency,omitempty"` // USD
 	Details  AmountDetails `json:"details,omitempty"`
 }
 
@@ -45,6 +64,8 @@ type AmountDetails struct {
 
 type PaymentOptions struct {
 	AllowedPaymentMethod string `json:"allowed_payment_method,omitempty"`
+	RecurringFlag        bool   `json:"recurring_flag,omitempty"`
+	SkipFmf              bool   `json:"skip_fmf,omitempty"`
 }
 
 type ItemList struct {
@@ -53,13 +74,13 @@ type ItemList struct {
 }
 
 type Item struct {
-	Name        string `json:"name,omitempty"`
-	Description string `json:"description,omitempty"`
-	Quantity    string `json:"quantity,omitempty"`
-	Price       string `json:"price,omitempty"`
-	Tax         string `json:"tax,omitempty"`
-	Sku         string `json:"sku,omitempty"`
-	Currency    string `json:"currency,omitempty"`
+	Name        string      `json:"name,omitempty"`
+	Description string      `json:"description,omitempty"`
+	Quantity    interface{} `json:"quantity,omitempty"` // input is string, paypal return is usually int
+	Price       string      `json:"price,omitempty"`
+	Tax         string      `json:"tax,omitempty"`
+	Sku         string      `json:"sku,omitempty"`
+	Currency    string      `json:"currency,omitempty"`
 }
 
 type ShippingAddress struct {
@@ -82,4 +103,32 @@ type Link struct {
 	Href   string `json:"href,omitempty"`
 	Rel    string `json:"rel,omitempty"`
 	Method string `json:"method,omitempty"`
+}
+
+type ErrorDetails struct {
+	Field string `json:"field,omitempty"`
+	Issue string `json:"issue,omitempty"`
+}
+
+type RelatedResource struct {
+	Sale Sale `json:"sale,omitempty"`
+}
+
+type Sale struct {
+	Id                        string         `json:"id,omitempty"`
+	CreateTime                string         `json:"create_time,omitempty"`
+	UpdateTime                string         `json:"update_time,omitempty"`
+	Amount                    Amount         `json:"amount,omitempty"`
+	PaymentMode               string         `json:"payment_mode,omitempty"`
+	State                     string         `json:"state,omitempty"`
+	ProtectionEligibility     string         `json:"protection_eligibility,omitempty"`
+	ProtectionEligibilityType string         `json:"protection_eligibility_type,omitempty"`
+	TransactionFee            TransactionFee `json:"transaction_fee,omitempty"`
+	ParentPayment             string         `json:"parent_payment,omitempty"`
+	Links                     []Link         `json:"links,omitempty"`
+}
+
+type TransactionFee struct {
+	Value    string `json:"value,omitempty"`
+	Currency string `json:"currency,omitempty"`
 }
--- module/paypal/paypal.go
+++ module/paypal/paypal.go
@@ -14,7 +14,6 @@ import (
 	"net/url"
 	"strings"
 	"strconv"
-	"io"
 )
 
 type paypal interface {
@@ -23,16 +22,18 @@ type paypal interface {
 }
 
 type PayPal struct {
-	router        *mux.Router
-	SandboxMode   bool
-	Client        string
-	Key           string
-	SandboxClient string
-	SandboxKey    string
-	apiUrl        string
-	credentials   *credentials
+	router           *mux.Router
+	SandboxMode      bool
+	AppHostname      string
+	Client           string
+	Key              string
+	SandboxClient    string
+	SandboxKey       string
+	apiUrl           string
+	credentials      *credentials
 	module.Module
 	paypal
+	OnPaymentCreated func(ctx context.Context, paymentId string, payerId string, token string) (redirectUrl string)
 }
 
 type credentials struct {
@@ -57,6 +58,9 @@ func (p *PayPal) Init() error {
 		}
 		p.apiUrl = "https://api.paypal.com/v1/"
 	}
+	if len(p.AppHostname) == 0 {
+		return errors.New("AppHostname must not be empty")
+	}
 	return nil
 }
 
@@ -68,10 +72,10 @@ func (p *PayPal) Router(modulePath string) *mux.Router {
 	if p.router == nil {
 		p.router = mux.NewRouter()
 		// add callbacks
+
+		// test order
 		p.router.HandleFunc(modulePath+"/hi", func(writer http.ResponseWriter, request *http.Request) {
 			ctx := appengine.NewContext(request)
-
-			moduleHost, _ := appengine.ModuleHostname(ctx, "", "", "")
 
 			payment, err := p.CreatePayment(ctx, &Payment{
 				Intent: "sale",
@@ -79,17 +83,15 @@ func (p *PayPal) Router(modulePath string) *mux.Router {
 				Transactions: []Transaction{
 					{
 						Amount: Amount{
-							Total:    "30.11",
+							Total:    "30.00",
 							Currency: "EUR",
 							Details: AmountDetails{
 								Subtotal: "30.00",
-								Tax:      "0.07",
-								Shipping: "0.03",
 							},
 						},
 						Description:    "The payment transaction description.",
 						Custom:         "ROCKETBOOK_EMS_90048630024435",
-						InvoiceNumber:  "2314324234",
+						InvoiceNumber:  strconv.Itoa(int(time.Now().Unix())),
 						PaymentOptions: PaymentOptions{AllowedPaymentMethod: "INSTANT_FUNDING_SOURCE"},
 						SoftDescriptor: "ECHI5786786",
 						ItemList: ItemList{
@@ -97,33 +99,51 @@ func (p *PayPal) Router(modulePath string) *mux.Router {
 								{
 									Name:        "hat",
 									Description: "Brown hat.",
-									Quantity:    "2",
-									Price:       "3",
-									Tax:         "0.01",
+									Quantity:    "1",
+									Price:       "30.00",
 									Sku:         "1",
 									Currency:    "EUR",
 								},
+							},
+							ShippingAddress: ShippingAddress{
+								RecipientName: "Test Test",
+								Line1:         "4thFloot",
+								Line2:         "unit#43",
+								City:          "San Jose",
+								CountryCode:   "US",
+								PostalCode:    "95131",
+								Phone:         "011862212345678",
+								State:         "CA",
 							},
 						},
 					},
 				},
 				NoteToPayer: "Contact us for any questions on your order.",
 				RedirectUrls: RedirectUrls{
-					ReturnUrl: moduleHost + modulePath + "/return",
-					CancelUrl: moduleHost + modulePath + "/cancel",
+					ReturnUrl: "https://" + p.AppHostname + modulePath + "/return",
+					CancelUrl: "https://" + p.AppHostname + modulePath + "/cancel",
 				},
 			})
 			if err != nil {
 				writer.Write([]byte(err.Error()))
 				return
 			}
-			writer.Write([]byte(payment))
+			json.NewEncoder(writer).Encode(payment)
+			//writer.Write(payment.Bytes())
 		})
 		p.router.HandleFunc(modulePath+"/return", func(writer http.ResponseWriter, request *http.Request) {
-			writer.Write([]byte("return"))
+			if p.OnPaymentCreated != nil {
+				query := request.URL.Query()
+				redirectUrl := p.OnPaymentCreated(appengine.NewContext(request), query.Get("paymentId"), query.Get("token"), query.Get("PayerID"))
+				if len(redirectUrl) > 0 {
+					http.Redirect(writer, request, redirectUrl, http.StatusSeeOther)
+				}
+			}
 		})
 		p.router.HandleFunc(modulePath+"/cancel", func(writer http.ResponseWriter, request *http.Request) {
-			writer.Write([]byte("cancel"))
+			buf := new(bytes.Buffer)
+			buf.ReadFrom(request.Body)
+			writer.Write(buf.Bytes())
 		})
 	}
 	return p.router
@@ -168,7 +188,7 @@ func (p *PayPal) auth(ctx context.Context) error {
 	return nil
 }
 
-func (p *PayPal) CreatePayment(ctx context.Context, payment *Payment) (io.Reader, error) {
+func (p *PayPal) CreatePayment(ctx context.Context, payment *Payment) (*Payment, error) {
 	if err := p.auth(ctx); err != nil {
 		return nil, err
 	}
@@ -192,12 +212,39 @@ func (p *PayPal) CreatePayment(ctx context.Context, payment *Payment) (io.Reader
 	if err != nil {
 		return nil, err
 	}
-	return buf, nil
-	/*responsePayment := new(Payment)
+	//return buf, nil
+	responsePayment := new(Payment)
 	err = json.Unmarshal(buf.Bytes(), responsePayment)
 	if err != nil {
 		return nil, err
 	}
+	return responsePayment, nil
+}
 
-	return responsePayment, nil*/
+func (p *PayPal) ExecutePayment(ctx context.Context, paymentId string, payerId string, token string) (*Payment, error) {
+	body := map[string]string{
+		"payer_id": payerId,
+	}
+	bsBody, _ := json.Marshal(body)
+	req, _ := http.NewRequest(http.MethodPost, p.apiUrl+"payments/payment/"+paymentId+"/execute", bytes.NewReader(bsBody))
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Authorization", "Bearer "+token)
+
+	client := urlfetch.Client(ctx)
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	buf := new(bytes.Buffer)
+	_, err = buf.ReadFrom(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	//return buf, nil
+	responsePayment := new(Payment)
+	err = json.Unmarshal(buf.Bytes(), responsePayment)
+	if err != nil {
+		return nil, err
+	}
+	return responsePayment, nil
 }
